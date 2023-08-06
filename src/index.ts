1398de5475db73119194269cfa7667b8b0c8fa6d
Rob Mayer <5171361+RobMayer@users.noreply.github.com>
2023-08-06 13:46:15 -0400

    messed up project version

--- src/index.ts
+++ src/index.ts
@@ -112,22 +112,6 @@ const buildProject = async () => {
             Logger.error("Could not create workspace directory");
             throw e;
         }
-        Logger.log("copying template...");
-        try {
-            await fs.cp(templateDir, projectDir, { recursive: true });
-            await fs.rename(path.join(projectDir, "gitignore"), path.join(projectDir, ".gitignore"));
-            await fs.rename(path.join(projectDir, "template.package.json"), path.join(projectDir, "package.json"));
-            if (template === "typescript") {
-                await fs.rename(path.join(projectDir, "template.tsconfig.json"), path.join(projectDir, "tsconfig.json"));
-            }
-            const projectPackage = JSON.parse(await fs.readFile(path.join(projectDir, "package.json"), "utf-8"));
-            projectPackage.name = projectSlug;
-            await fs.writeFile(path.join(projectDir, "package.json"), JSON.stringify(projectPackage, null, 2));
-            Logger.success("template copied");
-        } catch (e) {
-            Logger.error("could not copy template");
-            throw e;
-        }
 
         const suggestedPrdGuid = guid();
         const inputPrdGuid = autoConfirm
@@ -144,6 +128,27 @@ const buildProject = async () => {
             ? suggestedTTPGPath
             : (await input.question(chalk.whiteBright(`path to TTPG Package or PersistentDownloadDir directory ${suggestedTTPGPath ? `[${chalk.white(suggestedTTPGPath)}]: ` : ": "}`))).trim();
         const ttpg_path = input_ttpg_path !== "" ? input_ttpg_path : suggestedTTPGPath;
+        const suggestedVersion = "0.0.1";
+        const inputVersion = autoConfirm ? suggestedVersion : await input.question(chalk.whiteBright("provide a version number [0.0.1]: "));
+        const projectVersion = inputVersion !== "" ? inputVersion : suggestedVersion;
+
+        Logger.log("copying template...");
+        try {
+            await fs.cp(templateDir, projectDir, { recursive: true });
+            await fs.rename(path.join(projectDir, "gitignore"), path.join(projectDir, ".gitignore"));
+            await fs.rename(path.join(projectDir, "template.package.json"), path.join(projectDir, "package.json"));
+            if (template === "typescript") {
+                await fs.rename(path.join(projectDir, "template.tsconfig.json"), path.join(projectDir, "tsconfig.json"));
+            }
+            const projectPackage = JSON.parse(await fs.readFile(path.join(projectDir, "package.json"), "utf-8"));
+            projectPackage.name = projectSlug;
+            projectPackage.version = projectVersion;
+            await fs.writeFile(path.join(projectDir, "package.json"), JSON.stringify(projectPackage, null, 2));
+            Logger.success("template copied");
+        } catch (e) {
+            Logger.error("could not copy template");
+            throw e;
+        }
 
         input.close();
         Logger.log("creating asset directories...");
@@ -156,13 +161,13 @@ const buildProject = async () => {
         }
         Logger.log("creating dev directory...");
         try {
-            await fs.mkdir(path.join(projectDir, "dev", projectSlug), { recursive: true });
+            await fs.mkdir(path.join(projectDir, "dev", `${projectSlug}_dev`), { recursive: true });
             await fs.writeFile(
-                path.join(projectDir, "dev", projectSlug, "Manifest.json"),
+                path.join(projectDir, "dev", `${projectSlug}_dev`, "Manifest.json"),
                 JSON.stringify(
                     {
                         Name: `${projectTitle} (Dev)`,
-                        Version: "0.0.1",
+                        Version: projectVersion,
                         GUID: devGuid,
                     },
                     null,
@@ -170,7 +175,7 @@ const buildProject = async () => {
                 ),
                 "utf-8"
             );
-            await Promise.all(ASSET_DIRS.map((dir) => fs.symlink(path.join(projectDir, "assets", dir), path.join(projectDir, "dev", projectSlug, dir), "junction")));
+            await Promise.all(ASSET_DIRS.map((dir) => fs.symlink(path.join(projectDir, "assets", dir), path.join(projectDir, "dev", `${projectSlug}_dev`, dir), "junction")));
             Logger.success("dev directory created");
         } catch (e) {
             Logger.error("could not create dev directory");
@@ -184,6 +189,7 @@ const buildProject = async () => {
                     {
                         name: projectTitle,
                         slug: projectSlug,
+                        version: projectVersion,
                         template,
                         guid: {
                             dev: devGuid,
